09941bbe61aac3b6f6b7a748e7cddfe313f802b4
taratimmerman <timmermantara@gmail.com>
2021-02-13 21:12:20 -0700

    sidebar

--- fb-clone/src/App.js
+++ fb-clone/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './Header';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
@@ -7,9 +8,8 @@ function App() {
     <div className="app">
       <Header />
       
-      <div>
-        {/* App Body */}
-        {/* Sidebar */}
+      <div className='app_body'>
+        <Sidebar />
         {/* Feed */}
         {/* Widgets */}
       </div>
