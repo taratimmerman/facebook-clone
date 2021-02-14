11396aa53598245b2efc0bda208f321b6c488b66
taratimmerman <timmermantara@gmail.com>
2021-02-13 18:54:11 -0700

    fb clone initial map

---
+++ fb-clone/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    // BEM naming convention
+    <div className="app">
+      <h1>fb clone</h1>
+
+          {/* Header */}
+
+          {/* App Body */}
+            {/* Sidebar */}
+            {/* Feed */}
+            {/* Widgets */}
+    </div>
+  );
+}
+
+export default App;
---
+++ fb-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCKt49tNi8eJRZLQg7-icf4acQ9LKe4ue8",
+    authDomain: "facebook-clone-6658e.firebaseapp.com",
+    projectId: "facebook-clone-6658e",
+    storageBucket: "facebook-clone-6658e.appspot.com",
+    messagingSenderId: "624890266065",
+    appId: "1:624890266065:web:795c19fa30870863f720f0",
+    measurementId: "G-JM9KR0SH3P"
+  };
