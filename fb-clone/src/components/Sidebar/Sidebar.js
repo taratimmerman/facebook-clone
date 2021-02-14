1de463940bd3179b0d40dfe1f8ba434b5db2f056
taratimmerman <timmermantara@gmail.com>
2021-02-14 15:41:00 -0700

    status

--- fb-clone/src/components/Sidebar/Sidebar.js
+++ fb-clone/src/components/Sidebar/Sidebar.js
@@ -8,11 +8,14 @@ import ChatIcon from '@material-ui/icons/Chat'
 import StoreFrontIcon from '@material-ui/icons/Storefront'
 import VideoLibraryIcon from '@material-ui/icons/VideoLibrary'
 import { ExpandMoreOutlined } from '@material-ui/icons'
+import { useStateValue } from '../Login/StateProvider'
 
 const Sidebar = () => {
+    const [{ user }, dispatch] = useStateValue();
+
     return (
         <div className='sidebar'>
-            <SidebarRow src='https://avatars.githubusercontent.com/u/42617001?s=460&u=009e72391064be844aa51ed629a8cdd50b400166&v=4' title='Tara Timmerman'/>
+            <SidebarRow src={user.photoURL} title={user.displayName}/>
             <SidebarRow Icon={LocalHospitalIcon} title='COVID-19 Information Center'/>
             <SidebarRow Icon={EmojiFlagsIcon} title='Pages'/>
             <SidebarRow Icon={PeopleIcon} title='Friends'/>
