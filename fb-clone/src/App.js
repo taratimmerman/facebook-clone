3d2eb23bf146b28bbfd1684f762f059b05a71dd8
taratimmerman <timmermantara@gmail.com>
2021-02-13 20:21:56 -0700

    header

--- fb-clone/src/App.js
+++ fb-clone/src/App.js
@@ -1,17 +1,18 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     // BEM naming convention
     <div className="app">
-      <h1>fb clone</h1>
-
-          {/* Header */}
-
-          {/* App Body */}
-            {/* Sidebar */}
-            {/* Feed */}
-            {/* Widgets */}
+      <Header />
+      
+      <div>
+        {/* App Body */}
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+      </div>
     </div>
   );
 }
